f8ed8a79ff4b5897d6d60bdca56215038ff9d0c2
Apex_Predator <94287474+Apex-Predator-786@users.noreply.github.com>
2024-04-25 15:28:16 +0530

    Create social.js

---
+++ js/social.js
@@ -0,0 +1,10 @@
+document.querySelector(".fa-telegram").addEventListener("click", telegram);
+
+function telegram() {
+    window.open("https://www.google.com", "_blank");
+}
+document.querySelector(".fa-github").addEventListener("click", github);
+
+function github() {
+    window.open("https://github.com/Apex-Predator-786/cc_generator", "_blank");
+}
